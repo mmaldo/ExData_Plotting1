d5874ee72c425b0b8b9093c576836a5eda827ba8
Matt <candyman2430@gmail.com>
2014-05-11 12:33:56 -0700

    Add plot 4

---
+++ plot4.R
@@ -0,0 +1,37 @@
+## Read in the power data
+powerData<-read.table("./data/exdata-data-household_power_consumption/household_power_consumption.txt",sep=";",header=TRUE,na.strings ="?")
+
+## Remove NA rows
+powerData<-na.omit(powerData)
+
+## Covert Date column data to Date objects
+powerData[,"Date"]<-as.Date(powerData$Date, format="%d/%m/%Y")
+
+## Subset data based on date range
+powerData<-powerData[powerData$Date== "2007-02-01" | powerData$Date== "2007-02-02",]
+
+datetime<-paste(powerData$Date, powerData$Time,sep=" ")
+
+powerData<-data.frame(powerData,datetime=strptime(datetime, "%Y-%m-%d %H:%M:%S"))
+
+
+## open png device
+png(filename= "plot4.png",width = 480, height = 480)
+
+## Create Plot
+## set to 2 plots per row and 2 columns
+par(mfrow=c(2,2))
+## Create plot of Global Active Power vs datetime
+plot(powerData$datetime,powerData$Global_active_power,type="l", lwd = 1,xlab="",ylab="Global Active Power (kilowatts)")
+## Create plot of Voltage vs datetime
+plot(powerData$datetime,powerData$Voltage,type="l", lwd = 1,xlab="datetime",ylab="Voltage")
+## Create plot of Sub_metering vs datetime
+plot(powerData$datetime,powerData$Sub_metering_1, type="l",ylab="Energy sub metering",xlab="")
+lines(powerData$datetime,powerData$Sub_metering_2,type="l",col="Red")
+lines(powerData$datetime,powerData$Sub_metering_3,type="l",col="Blue")
+legend("topright", legend= c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col = c("black","blue","red"), lwd=1)
+## Create plot of Global Reactive Power vs datetime
+plot(powerData$datetime,powerData$Global_reactive_power,type="l", lwd = 1,xlab="datetime",ylab="Global_reactive_power")
+
+## Close png device
+dev.off() 
